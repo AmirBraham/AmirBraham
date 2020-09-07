23538a190d71d1ab02582eedc94c57093e93b6c8
Amir Braham <amirbrahamm@gmail.com>
2020-09-07 22:00:42 +0100

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-120944925-2",
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
